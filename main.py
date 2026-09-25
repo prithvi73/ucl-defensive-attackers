c8f2cec6e363f7e748aa5077137635c84d712b03
Prithvisom Sinha <prithvisomsinha@gmail.com>
2026-09-25 08:04:06 +0530

    Fixed pandas filter and calculated top 10 defending attackers

--- main.py
+++ main.py
@@ -12,5 +12,25 @@ combined_df = pd.merge(defense_df, player_subset, on='id_player')
 # print(combined_df.head())
 
 # # getting the players who are forwards for our actual dataframe
-df = combined_df[combined_df["field_position"] == "Forward"]
-print(df.head())
+df = combined_df[combined_df["field_position"] == "Forward"].copy()
+# print(df.to_string())
+
+# calculating top attacking defenders
+df["tackle_success_pct"] = (df["tackles_won"] / df["tackles"]) * 100
+df["tackle_success_pct"] = df["tackle_success_pct"].fillna(0)
+# print(df["tackle_success_pct"]) 
+
+df["total_defensive_impact"] = (df["tackles_won"] + df["balls_recovered"])
+
+# player must have attempted atleast 5 tackles or recovered 5 balls
+qualified_attackers = df[(df["tackles"] >= 5) | (df["balls_recovered"] >= 5)]
+
+top_defending_attackers = qualified_attackers.sort_values(by="total_defensive_impact", ascending=False)
+
+print(top_defending_attackers[["player_name", "total_defensive_impact", "tackle_success_pct"]].head(10))
+
+
+
+
+
+
